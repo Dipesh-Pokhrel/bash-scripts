7a72c60b738c5fd6c09cc4c0ef6cc82940a12a54
root <root@LAPTOP-U6GDKQN9.localdomain>
2022-07-31 08:55:02 +0545

    if-else

--- if.sh
+++ if.sh
@@ -7,3 +7,14 @@ if [[ $a -gt 4 ]]; then
 else
 	echo "$a is not greater than 4"
 fi
+
+
+# Arithmetic 
+
+if (( $a > 4)); then 
+	echo "$a is greater than 4"
+elif (( $a >2 )); then
+	echo "$a is greater than 2"
+else
+	echo "$a is not greater than 4"
+fi
